81a0d2aec1557dc3f823e9e21beca86999bd4965
Jay Chauhan <jayschauhan3@gmail.com>
2025-12-05 08:50:31 -0500

    added working vite project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SquareAwayLanding from './components/SquareAwayLanding'
+
+function App() {
+  return (
+    <div className="App">
+      <SquareAwayLanding />
+    </div>
+  )
+}
+
+export default App
